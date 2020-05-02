7a81d9a42a31f6a5924b713658f8c08c5748de41
chrisloarryn <cristobal.contreras11@inacapmail.cl>
2020-05-01 21:40:58 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom'
 
-function App() {
-  return <h1>Let's start!</h1>;
+// Import components
+import Users from './users/pages/Users'
+import NewPlace from './places/pages/NewPlace'
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+        <Route path='/places/new' exact>
+          <NewPlace />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/users/components/UsersList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+// Components
+import UserItem from './UserItem'
+
+// CSS Styles
+import './UsersList.css'
+const UsersList = props => {
+  if (props.items.length === 0) {
+    return (
+      <div className='center'>
+        <h2>No users found.</h2>
+      </div>
+    )
+  }
+  return (
+    <ul className='users-list'>
+      {props.items.map(user => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  )
+}
+
+export default UsersList
