9cf2234cac1a54fef32521d14770b171c76fce81
chrisloarryn <cristobal.contreras11@inacapmail.cl>
2020-05-03 00:24:55 -0400

    Create Map.js

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// Components
+
+// CSS Styles
+import './Map.css'
+
+const Map = () => {
+    return (
+        <div className={`map ${props.className}`} style={props.style} >
+            
+        </div>
+    )
+}
+
+export default Map
