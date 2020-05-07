42ac43bdc6245b87f5daaad4de56431dd336e05f
chrisloarryn <cristobal.contreras11@inacapmail.cl>
2020-05-07 01:01:09 -0400

    Update Modal.js

--- src/shared/components/UIElements/Modal.js
+++ src/shared/components/UIElements/Modal.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { CSSTransition } from 'react-transition-group';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { CSSTransition } from 'react-transition-group'
 
-import Backdrop from './Backdrop';
-import './Modal.css';
+import Backdrop from './Backdrop'
+import './Modal.css'
 
 const ModalOverlay = props => {
   const content = (
@@ -24,9 +24,9 @@ const ModalOverlay = props => {
         </footer>
       </form>
     </div>
-  );
-  return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
-};
+  )
+  return ReactDOM.createPortal(content, document.getElementById('modal-hook'))
+}
 
 const Modal = props => {
   return (
@@ -37,12 +37,12 @@ const Modal = props => {
         mountOnEnter
         unmountOnExit
         timeout={200}
-        classNames="modal"
+        classNames='modal'
       >
         <ModalOverlay {...props} />
       </CSSTransition>
     </React.Fragment>
-  );
-};
+  )
+}
 
-export default Modal;
+export default Modal
