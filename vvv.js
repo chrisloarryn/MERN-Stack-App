628564345f4addf799ca8907123cbfd0f50526e8
chrisloarryn <chrisloarryn@gmail.com>
2020-06-21 13:36:22 -0400

    Update vvv.js

--- vvv.js
+++ vvv.js
@@ -1,5 +1,11 @@
 function ff(){ 
     return "31" + 9
 };
+const arr = [223, 2, 34, 4, 9, 98, 905, 503];
 
-console.log(ff())
+function multiply(a, b) {
+    return a * b;
+}
+
+const multi = arr.reduce(multiply, 1);
+console.log('Quantity:', multi);
